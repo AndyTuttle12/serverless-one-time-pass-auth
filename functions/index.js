de1b20531d47875f4df7e9596b228a3d4a53a3fa
Andy Tuttle <andytuttle12@gmail.com>
2017-09-03 09:48:14 -0400

    Starting new authentication project using serverless functions.

---
+++ functions/index.js
@@ -0,0 +1,11 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const createUser = require('./create_user');
+const serviceAccount = require('./service_account.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://one-time-password-auth.firebaseio.com"
+});
+
+exports.createUser = functions.https.onRequest(createUser);
